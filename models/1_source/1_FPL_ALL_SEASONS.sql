f8244e427c49f9ce5e2d26a19e048eb43834c38f
George Soto <georgesoto@hotmail.co.uk>
2025-01-06 15:56:14 +0000

    FPL convert strings to int/float

--- models/1_source/1_FPL_ALL_SEASONS.sql
+++ models/1_source/1_FPL_ALL_SEASONS.sql
@@ -1,85 +1,85 @@
 WITH FPL_SEASON_2017 AS(
   SELECT
     NAME,
-    Clean_Sheets,
-    Goals_Conceded,
-    Own_Goals,
-    SAVES,
-    COST,
-    Points,
-    '2017' AS SEASON_START
+    CAST(Clean_Sheets AS INT64),
+    CAST(Goals_Conceded AS INT64),
+    CAST(Own_Goals AS INT64),
+    CAST(SAVES AS INT64),
+    CAST(COST AS FLOAT),
+    CAST(Points AS INT64),
+    2017 AS SEASON_START
   FROM {{ source('fpl_historic', '2017') }}),
 
 FPL_SEASON_2018 AS(
   SELECT
     NAME,
-    Clean_Sheets,
-    Goals_Conceded,
-    Own_Goals,
-    SAVES,
-    COST,
-    Points,
-    '2018' AS SEASON_START
+    CAST(Clean_Sheets AS INT64),
+    CAST(Goals_Conceded AS INT64),
+    CAST(Own_Goals AS INT64),
+    CAST(SAVES AS INT64),
+    CAST(COST AS FLOAT),
+    CAST(Points AS INT64),
+    2018 AS SEASON_START
   FROM {{ source('fpl_historic', '2018') }}),
 
 FPL_SEASON_2019 AS(
   SELECT
     NAME,
-    Clean_Sheets,
-    Goals_Conceded,
-    Own_Goals,
-    SAVES,
-    COST,
-    Points,
-    '2019' AS SEASON_START
+    CAST(Clean_Sheets AS INT64),
+    CAST(Goals_Conceded AS INT64),
+    CAST(Own_Goals AS INT64),
+    CAST(SAVES AS INT64),
+    CAST(COST AS FLOAT),
+    CAST(Points AS INT64),
+    2019 AS SEASON_START
   FROM {{ source('fpl_historic', '2019') }}),
 
 FPL_SEASON_2020 AS(
   SELECT
     NAME,
-    Clean_Sheets,
-    Goals_Conceded,
-    Own_Goals,
-    SAVES,
-    COST,
-    Points,
-    '2020' AS SEASON_START
+    CAST(Clean_Sheets AS INT64),
+    CAST(Goals_Conceded AS INT64),
+    CAST(Own_Goals AS INT64),
+    CAST(SAVES AS INT64),
+    CAST(COST AS FLOAT),
+    CAST(Points AS INT64),
+    2020 AS SEASON_START
   FROM {{ source('fpl_historic', '2020') }}),
 
 FPL_SEASON_2021 AS(
   SELECT
     NAME,
-    Clean_Sheets,
-    Goals_Conceded,
-    Own_Goals,
-    SAVES,
-    COST,
-    Points,
-    '2021' AS SEASON_START
+    CAST(Clean_Sheets AS INT64),
+    CAST(Goals_Conceded AS INT64),
+    CAST(Own_Goals AS INT64),
+    CAST(SAVES AS INT64),
+    CAST(COST AS FLOAT),
+    CAST(Points AS INT64),
+    2021 AS SEASON_START
   FROM {{ source('fpl_historic', '2021') }}),
 
 FPL_SEASON_2022 AS(
   SELECT
     NAME,
-    Clean_Sheets,
-    Goals_Conceded,
-    Own_Goals,
-    SAVES,
-    COST,
-    Points,
-    '2022' AS SEASON_START
+    CAST(Clean_Sheets AS INT64),
+    CAST(Goals_Conceded AS INT64),
+    CAST(Own_Goals AS INT64),
+    CAST(SAVES AS INT64),
+    CAST(COST AS FLOAT),
+    CAST(Points AS INT64),
+    2022 AS SEASON_START
   FROM {{ source('fpl_historic', '2022') }}),
 
 FPL_SEASON_2023 AS(
   SELECT
     NAME,
-    Clean_Sheets,
-    Goals_Conceded,
-    Own_Goals,
-    SAVES,
-    COST,
-    Points,
-    '2023' AS SEASON_START
+    CAST(Clean_Sheets AS INT64),
+    CAST(Goals_Conceded AS INT64),
+    CAST(Own_Goals AS INT64),
+    CAST(SAVES AS INT64),
+    CAST(COST AS FLOAT),
+    CAST(Points AS INT64),
+    2023 AS SEASON_START
   FROM {{ source('fpl_historic', '2023') }})
 
 SELECT * FROM FPL_SEASON_2017
